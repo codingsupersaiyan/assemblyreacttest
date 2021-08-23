c24623c0a86448c486b5088d8b3d23fb16ac1dcb
Jay Hongvarivatana <jayyhong@gmail.com>
2021-08-23 15:18:30 -0700

    Added details such as awards, author, and votes

--- src/views/ImageFocus/ImageFocus.js
+++ src/views/ImageFocus/ImageFocus.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import classes from "./ImageFocus.module.css";
+import thumbsUp from "../../assets/images/thumb-up.png";
 
 export default function ImageFocus(props) {
   useEffect(() => {
@@ -17,17 +18,32 @@ export default function ImageFocus(props) {
         </div>
         <div className={classes.rightContainer}>
           <div className={classes.imageAuthor}>{props.data.data.author}</div>
+          <div className={classes.imageCategories}>
+            {props.data.data.content_categories.map((category) => {
+              return <span>#{category}</span>;
+            })}
+          </div>
           <div className={classes.awardsContainer}>
             {props.data.data.all_awardings.map((award) => {
               return (
                 <span>
-                <img src={award.icon_url} className={classes.awardIcon}></img>
-                <div className={classes.awardCount}>{award.count}</div>
+                  <img src={award.icon_url} className={classes.awardIcon}></img>
+                  <div className={classes.awardCount}>{award.count}</div>
                 </span>
               );
             })}
           </div>
           <div className={classes.imageTitle}>"{props.data.data.title}"</div>
+          <div className={classes.votesContainer}>
+            <div>
+              <img className={classes.thumbs} src={thumbsUp} />
+              <span className={classes.votes}>{props.data.data.ups}</span>
+            </div>
+            <div>
+              <img className={`${classes.thumbs} ${classes.thumbsDown}`} src={thumbsUp} />
+              <span className={classes.votes}>{props.data.data.downs}</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
